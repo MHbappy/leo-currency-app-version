b89110478c71dd978f84257ad8fce7e3733d9753
MHbappy <mhbappy18@gmail.com>
2023-02-07 08:58:15 +0600

    added profile

--- create-package.sh
+++ create-package.sh
@@ -1,4 +1,4 @@
-./mvnw clean install
+./mvnw clean install -Pprod
 docker rmi -f mhbappy18/app_version_application &>/dev/null && echo 'Removed old container'
 docker build -t mhbappy18/app_version_application .
 docker push mhbappy18/app_version_application
